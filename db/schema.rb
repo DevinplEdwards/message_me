6938888e87d7b9d301789057ae3ef8377b76969e
devinple <devinpledwards@live.com>
2022-08-16 19:58:49 +0000

    add message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_184338) do
+ActiveRecord::Schema.define(version: 2022_08_16_191728) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
