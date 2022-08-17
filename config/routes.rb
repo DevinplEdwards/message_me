6eb9538c90fbad8ccd13b634efb3813c398ab1a5
devinple <devinpledwards@live.com>
2022-08-17 21:56:11 +0000

    add fix to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
-  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
